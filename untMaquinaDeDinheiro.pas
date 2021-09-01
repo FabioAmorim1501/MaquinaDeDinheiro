f2ce91f8cea2978b27087b8e925e242ec5783055
Fabio Amorim <fabioamorim1501@gmail.com>
2021-09-01 11:45:07 -0300

    Primeiro commit, versão incial da solução

---
+++ untMaquinaDeDinheiro.pas
@@ -0,0 +1,132 @@
+unit untMaquinaDeDinheiro;
+
+interface
+
+uses Classes;
+
+type
+  IMaquina = interface['{214BF960-EAF5-41C2-91D5-0E8F401D65A3}']
+    function MontarTroco(Troco: Double): TList;
+  end;
+
+  TCedula = (ceNota100, ceNota50, ceNota20, ceNota10, ceNota5, ceNota2,
+    ceMoeda100, ceMoeda50, ceMoeda25, ceMoeda10, ceMoeda5, ceMoeda1);
+
+  TTroco = class(TInterfacedObject, IMaquina)
+    function MontarTroco(Troco: Double): TList;
+  private
+    FTipo: TCedula;
+    FQuantidade: Integer;
+  public
+    constructor Create(Tipo: TCedula; Quantidade: Integer);
+    function GetTipo: TCedula;
+    function GetQuantidade: Integer;
+    procedure SetQuantidade(Quantidade: Integer);
+  end;
+
+implementation
+
+uses
+  SysUtils;
+
+{ TTroco }
+
+constructor TTroco.Create(Tipo: TCedula; Quantidade: Integer);
+begin
+  inherited Create();
+  FTipo := Tipo;
+  FQuantidade := Quantidade;
+end;
+
+function TTroco.GetQuantidade: Integer;
+begin
+  Result := FQuantidade;
+end;
+
+function TTroco.GetTipo: TCedula;
+begin
+  Result := FTipo;
+end;
+
+function TTroco.MontarTroco(Troco: Double): TList;
+var
+  ListaTroco: TList;
+  ValorTrocoRestante: Double;
+begin
+  if Troco < 0 then
+  begin
+    raise Exception.Create('O troco não deve ser negativo, verificar chamada do método MontarTroco!');
+  end;
+  ValorTrocoRestante := Troco;
+  ListaTroco := TList.Create;
+
+  if ValorTrocoRestante >= 100 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceNota100, Round(ValorTrocoRestante) div 100));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 100) / 100;
+  end;
+  if ValorTrocoRestante >= 50 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceNota50, Round(ValorTrocoRestante) div 50));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 50) / 100;
+  end;
+  if ValorTrocoRestante >= 20 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceNota20, Round(ValorTrocoRestante) div 20));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 20) / 100;
+  end;
+  if ValorTrocoRestante >= 10 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceNota10, Round(ValorTrocoRestante) div 10));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 10) / 100;
+  end;
+  if ValorTrocoRestante >= 5 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceNota5, Round(ValorTrocoRestante) div 5));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 5) / 100;
+  end;
+  if ValorTrocoRestante >= 2 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceNota2, Round(ValorTrocoRestante) div 2));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 2) / 100;
+  end;
+
+  ValorTrocoRestante := ValorTrocoRestante * 100; { Converte o valor restante em centavos }
+  if ValorTrocoRestante >= 100 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceMoeda100, Round(ValorTrocoRestante) div 100));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 100) / 100;
+  end;
+  if ValorTrocoRestante >= 50 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceMoeda50, Round(ValorTrocoRestante) div 50));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 50) / 100;
+  end;
+  if ValorTrocoRestante >= 25 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceMoeda25, Round(ValorTrocoRestante) div 25));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 25) / 100;
+  end;
+  if ValorTrocoRestante >= 10 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceMoeda10, Round(ValorTrocoRestante) div 10));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 10) / 100;
+  end;
+  if ValorTrocoRestante >= 5 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceMoeda5, Round(ValorTrocoRestante) div 5));
+    ValorTrocoRestante := (Round(ValorTrocoRestante * 100) mod 5) / 100;
+  end;
+  if ValorTrocoRestante >= 1 then
+  begin
+    ListaTroco.Add(TTroco.Create(ceMoeda1, Round(ValorTrocoRestante) div 1));
+  end;
+  Result := ListaTroco;
+end;
+
+procedure TTroco.SetQuantidade(Quantidade: Integer);
+begin
+  FQuantidade := Quantidade;
+end;
+
+end.
